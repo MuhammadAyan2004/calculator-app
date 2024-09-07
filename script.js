f930d06cb396f8d3386d66ebef06303ed0d14315
Muhammad AYAN <m101ayan92@gmail.com>
2024-09-07 19:59:09 +0500

    update

--- script.js
+++ script.js
@@ -1,75 +1,39 @@
 let input = document.getElementById('inputBox');
+
 let buttons = document.querySelectorAll('button');
 
 let str = "";
+
 let arr = Array.from(buttons);
 
 arr.forEach(button =>{
+
     button.addEventListener("click",(e)=>{
         if (e.target.innerHTML == '=') {
+            
             str= eval(str);
             input.value = str;
         }
+
         else if (e.target.innerHTML == 'Ac'){
+
             str = "";
             input.value = "";
         }
+
         else if (e.target.innerHTML == 'Del'){
+
             str = str.substring(0,str.length - 1);
             input.value= str;
 
         }
+
         else{
             str += e.target.innerHTML;
             input.value = str;
+
         }
 
     });
+
 });
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
